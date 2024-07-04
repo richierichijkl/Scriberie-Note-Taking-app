53c4ee3c5306bd3024d5132ff21672a14cc59a31
richie <rc28122003@gmail.com>
2024-07-04 11:36:24 +0530

    uploaded file

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import './App.css';
+import { useState } from 'react';
+import uuid from "react-uuid";
+import Sidebar from './Sidebar';
+import Main  from './Main';
+
+
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  const [activeNote, setActiveNote] = useState(false);
+
+  
+  const onAddNote =() => {
+    const newNote = {
+      id:uuid(),
+      title: "Untitled Note",
+      body:"",
+      lastModified: Date.now(),
+    };
+
+    setNotes([newNote, ...notes]);
+  };
+
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArray = notes.map((note) => {
+      if(note.id === activeNote) {
+        return updatedNote;
+      }
+
+      return note;
+    });
+
+    setNotes(updatedNotesArray);
+  };
+
+  const onDeleleNote=(idToDelete) => {
+    setNotes(notes.filter((note) => note.id !== idToDelete));
+
+  };
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id===activeNote);
+  };
+
+
+
+  return (
+    <div className="App">
+      <Sidebar notes={notes} 
+      onAddNote={onAddNote} 
+      onDeleleNote={onDeleleNote}
+      activeNote={activeNote}
+      setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote}/>
+    </div>
+  );
+}
+
+export default App;
